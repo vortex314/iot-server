a810389b2b36abdc6b1d55afa8e6105e3bbc9843
lieven <lieven.merckx@gmail.com>
2023-07-22 15:42:20 +0200

    udp2redis proof

--- udp2redis.sh
+++ udp2redis.sh
@@ -1,1 +1,1 @@
-docker run --network host --name udp2redis  --restart=always -v $HOME/data/udp2redis:/data vortex314/udp2redis:latest
+docker run -d --network host --name udp2redis  --restart=always -v $HOME/data/udp2redis:/data lieven/udp2redis:latest
